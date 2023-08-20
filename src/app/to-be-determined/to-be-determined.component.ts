fd090c728aa2809bfa0767bd5a29c16fde21590f
Lucas Bledsoe <lbledsoe34@gmail.com>
2023-08-20 00:23:51 -0400

    fixed animations

--- src/app/to-be-determined/to-be-determined.component.ts
+++ src/app/to-be-determined/to-be-determined.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ToBeDeterminedComponent {
 
+  
+  ngOnInit(){
+  document.getElementById ("slideRight")!.addEventListener ("click", this.myFunction, false);
+  }
+  myFunction():void{
+    console.log("here");
+    if(document.getElementById("h31")?.className == "h3"){
+    document.getElementById("h31")?.classList.remove("h3");
+    document.getElementById("h31")?.classList.add("new");
+  document.getElementById("slideRight")!.innerHTML = "Slide Down"}
+    else{
+      document.getElementById("h31")?.classList.remove("new");
+      document.getElementById("h31")?.classList.add("h3");
+      document.getElementById("slideRight")!.innerHTML = "Slide Right";
+    
+  }
+}
 }
